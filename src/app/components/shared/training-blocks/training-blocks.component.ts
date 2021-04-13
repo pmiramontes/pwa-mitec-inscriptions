9709df8ceb2316409600e39b6ab2c7cc9d533c0c
Ivan Hernandez <ivanhdzrod10@gmail.com>
2021-04-12 19:49:49 -0500

    added training units selection screen

---
+++ src/app/components/shared/training-blocks/training-blocks.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'training-blocks',
+  templateUrl: './training-blocks.component.html',
+  styleUrls: ['./training-blocks.component.scss']
+})
+export class TrainingBlocksComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
